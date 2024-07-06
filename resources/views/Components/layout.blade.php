a29366dbcf2cc3dbafed46938523576494356799
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-06 10:43:27 +0700

    4: layout

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+
+<body>
+
+    <nav>
+        <x-navlink>
+            Home
+        </x-navlink>
+        <x-navlink>
+            About
+        </x-navlink>
+        <x-navlink>
+            Contact
+        </x-navlink>
+    </nav>
+
+    {{ $slot }}
+</body>
+
+</html>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>About</title>
-</head>
-<body>
+<x-layout>
   <h1>
-    About
+      About
   </h1>
-</body>
-</html>
+</x-layout>
