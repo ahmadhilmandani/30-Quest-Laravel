e26ae463f6e8660e4432edc61d13b913885192d8
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-09 09:10:26 +0700

    10 model factories

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
 class JobListing extends Model
 {
+  use HasFactory;
   protected $fillable = ['title', 'salary', 'description'];
+
+  public function employer()
+  {
+    return $this->belongsTo(Employer::class);
+  }
 }
