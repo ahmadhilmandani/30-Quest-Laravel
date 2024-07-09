94366f5963a2c986b1b868014fded30f360c36b1
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-09 07:16:20 +0700

    9: intro to eloquent

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,19 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use App\Models\JobListing;
 
 Route::get('/', function () {
+    dd(JobListing::all());
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['listJobs' => Job::all()]);
+    // return view('jobs', ['listJobs' => Job::all()]);
 });
 
 Route::get('/job/{id}', function ($id) {
-    return view('detail-job', ['job' => job::find($id)]);
+    // return view('detail-job', ['job' => job::find($id)]);
 });
 
 Route::get('/contact', function () {
